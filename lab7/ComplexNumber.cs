936800b4751c94cccd34c3a9c7a2d7128a9f2d70
qwert qwert <Dominikch19@gmail.com>
2023-11-14 18:38:45 +0100

    add ComplexNumber

---
+++ lab7/ComplexNumber.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab7
+{
+    class ComplexNumber
+    {
+        public double Real;
+        public double Imaginary;
+
+        public ComplexNumber(double real, double imaginary)
+        {
+            Real = real;
+            Imaginary = imaginary;
+        }
+
+        public static ComplexNumber operator +(ComplexNumber a, ComplexNumber b) {
+            return new ComplexNumber(a['r'] + b['r'], a['i'] + b['i']);
+        }
+
+        public static ComplexNumber operator -(ComplexNumber a, ComplexNumber b)
+        {
+            return new ComplexNumber(a['r'] - b['r'], a['i'] - b['i']);
+        }
+
+        public static ComplexNumber operator *(ComplexNumber a, ComplexNumber b)
+        {
+            return new ComplexNumber(a['r'] * b['r'] - a['i'] * b['i'], a['r'] * b['i'] + a['i'] * b['r']);
+        }
+
+        public static ComplexNumber operator /(ComplexNumber a, ComplexNumber b)
+        {
+            double denominator = b['r'] * b['r'] + b['i'] * b['i'];
+            ComplexNumber meter = a*( new ComplexNumber(b['r'], b['i']*-1));
+            meter['r'] /= denominator;
+            meter['i'] /= denominator;
+            return meter;
+        }
+
+        public static bool operator ==(ComplexNumber a, ComplexNumber b)
+        {
+            return a['r'] == b['r'] && a['i'] == b['i'];
+        }
+
+        public static bool operator !=(ComplexNumber a, ComplexNumber b)
+        {
+            return !(a == b);
+        }
+
+        public static bool operator >(ComplexNumber a, ComplexNumber b)
+        {
+            return Math.Sqrt(a['r'] * a['r'] + a['i'] + a['i']) > Math.Sqrt(b['r'] * b['r'] + b['i'] + b['i']);
+        }
+
+        public static bool operator <(ComplexNumber a, ComplexNumber b)
+        {
+            return Math.Sqrt(a['r'] * a['r'] + a['i'] + a['i']) < Math.Sqrt(b['r'] * b['r'] + b['i'] + b['i']);
+        }
+
+        public static explicit operator ComplexNumber(double number) {
+            return new ComplexNumber(number, 0);
+        }
+
+        public static implicit operator double(ComplexNumber number) {
+            return number['r'];
+        }
+
+        public double this[int index]
+        {
+            get 
+            {
+                if (index == 0)
+                    return Real;
+                else if (index == 1)
+                    return Imaginary;
+                else
+                    throw new ArgumentOutOfRangeException("Index 0 and 1 are only available");
+            }
+            set {
+                if (index == 0)
+                    Real = value;
+                else if (index == 1)
+                    Imaginary = value;
+                else
+                    throw new ArgumentOutOfRangeException("Index 0 and 1 are only available");
+            }
+            
+        }
+
+        public double this[char index]
+        {
+            get
+            {
+                if (index == 'r')
+                    return Real;
+                else if (index == 'i')
+                    return Imaginary;
+                else
+                    throw new ArgumentOutOfRangeException("Index 'r' and 'i' are only available");
+            }
+            set
+            {
+                if (index == 'r')
+                    Real = value;
+                else if (index == 'i')
+                    Imaginary = value;
+                else
+                    throw new ArgumentOutOfRangeException("Index 'r' and 'i' are only available");
+            }
+
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is ComplexNumber number &&
+                   Real == number.Real &&
+                   Imaginary == number.Imaginary;
+        }
+
+
+    }
+}
