7ab0ca722073168a806a99866ab133f930ec7358
qwert qwert <Dominikch19@gmail.com>
2023-11-14 19:08:20 +0100

    created gui

--- lab7/Form1.Designer.cs
+++ lab7/Form1.Designer.cs
@@ -29,13 +29,265 @@ namespace lab7
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.aRealTextBox = new System.Windows.Forms.TextBox();
+            this.aImaginaryTextBox = new System.Windows.Forms.TextBox();
+            this.operationLabel = new System.Windows.Forms.Label();
+            this.bRealTextBox = new System.Windows.Forms.TextBox();
+            this.bImaginaryTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.resultTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.additionButton = new System.Windows.Forms.Button();
+            this.substractionButton = new System.Windows.Forms.Button();
+            this.multiplicationButton = new System.Windows.Forms.Button();
+            this.divisionButton = new System.Windows.Forms.Button();
+            this.biggerDistanceButton = new System.Windows.Forms.Button();
+            this.samallerDistanceButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.hashButton = new System.Windows.Forms.Button();
+            this.equelsButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // aRealTextBox
+            // 
+            this.aRealTextBox.Location = new System.Drawing.Point(36, 56);
+            this.aRealTextBox.Name = "aRealTextBox";
+            this.aRealTextBox.Size = new System.Drawing.Size(100, 20);
+            this.aRealTextBox.TabIndex = 0;
+            // 
+            // aImaginaryTextBox
+            // 
+            this.aImaginaryTextBox.Location = new System.Drawing.Point(143, 55);
+            this.aImaginaryTextBox.Name = "aImaginaryTextBox";
+            this.aImaginaryTextBox.Size = new System.Drawing.Size(100, 20);
+            this.aImaginaryTextBox.TabIndex = 1;
+            // 
+            // operationLabel
+            // 
+            this.operationLabel.AutoSize = true;
+            this.operationLabel.Location = new System.Drawing.Point(249, 58);
+            this.operationLabel.Name = "operationLabel";
+            this.operationLabel.Size = new System.Drawing.Size(13, 13);
+            this.operationLabel.TabIndex = 2;
+            this.operationLabel.Text = "+";
+            // 
+            // bRealTextBox
+            // 
+            this.bRealTextBox.Location = new System.Drawing.Point(269, 55);
+            this.bRealTextBox.Name = "bRealTextBox";
+            this.bRealTextBox.Size = new System.Drawing.Size(100, 20);
+            this.bRealTextBox.TabIndex = 3;
+            // 
+            // bImaginaryTextBox
+            // 
+            this.bImaginaryTextBox.Location = new System.Drawing.Point(375, 56);
+            this.bImaginaryTextBox.Name = "bImaginaryTextBox";
+            this.bImaginaryTextBox.Size = new System.Drawing.Size(100, 20);
+            this.bImaginaryTextBox.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(481, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "=";
+            // 
+            // resultTextBox
+            // 
+            this.resultTextBox.Location = new System.Drawing.Point(501, 55);
+            this.resultTextBox.Name = "resultTextBox";
+            this.resultTextBox.Size = new System.Drawing.Size(98, 20);
+            this.resultTextBox.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(38, 37);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Rzeczywista";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(143, 37);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Urojona";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(269, 37);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(66, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Rzeczywista";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(375, 37);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(44, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Urojona";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(501, 37);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(37, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Wynik";
+            // 
+            // additionButton
+            // 
+            this.additionButton.Location = new System.Drawing.Point(36, 100);
+            this.additionButton.Name = "additionButton";
+            this.additionButton.Size = new System.Drawing.Size(75, 23);
+            this.additionButton.TabIndex = 14;
+            this.additionButton.Text = "+";
+            this.additionButton.UseVisualStyleBackColor = true;
+            // 
+            // substractionButton
+            // 
+            this.substractionButton.Location = new System.Drawing.Point(117, 100);
+            this.substractionButton.Name = "substractionButton";
+            this.substractionButton.Size = new System.Drawing.Size(75, 23);
+            this.substractionButton.TabIndex = 15;
+            this.substractionButton.Text = "-";
+            this.substractionButton.UseVisualStyleBackColor = true;
+            // 
+            // multiplicationButton
+            // 
+            this.multiplicationButton.Location = new System.Drawing.Point(198, 100);
+            this.multiplicationButton.Name = "multiplicationButton";
+            this.multiplicationButton.Size = new System.Drawing.Size(75, 23);
+            this.multiplicationButton.TabIndex = 16;
+            this.multiplicationButton.Text = "*";
+            this.multiplicationButton.UseVisualStyleBackColor = true;
+            // 
+            // divisionButton
+            // 
+            this.divisionButton.Location = new System.Drawing.Point(279, 100);
+            this.divisionButton.Name = "divisionButton";
+            this.divisionButton.Size = new System.Drawing.Size(75, 23);
+            this.divisionButton.TabIndex = 17;
+            this.divisionButton.Text = "/";
+            this.divisionButton.UseVisualStyleBackColor = true;
+            // 
+            // biggerDistanceButton
+            // 
+            this.biggerDistanceButton.Location = new System.Drawing.Point(36, 129);
+            this.biggerDistanceButton.Name = "biggerDistanceButton";
+            this.biggerDistanceButton.Size = new System.Drawing.Size(144, 23);
+            this.biggerDistanceButton.TabIndex = 18;
+            this.biggerDistanceButton.Text = "> (porównaj dystans)";
+            this.biggerDistanceButton.UseVisualStyleBackColor = true;
+            // 
+            // samallerDistanceButton
+            // 
+            this.samallerDistanceButton.Location = new System.Drawing.Point(186, 129);
+            this.samallerDistanceButton.Name = "samallerDistanceButton";
+            this.samallerDistanceButton.Size = new System.Drawing.Size(144, 23);
+            this.samallerDistanceButton.TabIndex = 19;
+            this.samallerDistanceButton.Text = "<  (porównaj dystans)";
+            this.samallerDistanceButton.UseVisualStyleBackColor = true;
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(522, 100);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(75, 23);
+            this.clearButton.TabIndex = 20;
+            this.clearButton.Text = "C";
+            this.clearButton.UseVisualStyleBackColor = true;
+            // 
+            // hashButton
+            // 
+            this.hashButton.Location = new System.Drawing.Point(441, 100);
+            this.hashButton.Name = "hashButton";
+            this.hashButton.Size = new System.Drawing.Size(75, 23);
+            this.hashButton.TabIndex = 21;
+            this.hashButton.Text = "hash";
+            this.hashButton.UseVisualStyleBackColor = true;
+            // 
+            // equelsButton
+            // 
+            this.equelsButton.Location = new System.Drawing.Point(360, 100);
+            this.equelsButton.Name = "equelsButton";
+            this.equelsButton.Size = new System.Drawing.Size(75, 23);
+            this.equelsButton.TabIndex = 22;
+            this.equelsButton.Text = "=";
+            this.equelsButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(630, 183);
+            this.Controls.Add(this.equelsButton);
+            this.Controls.Add(this.hashButton);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.samallerDistanceButton);
+            this.Controls.Add(this.biggerDistanceButton);
+            this.Controls.Add(this.divisionButton);
+            this.Controls.Add(this.multiplicationButton);
+            this.Controls.Add(this.substractionButton);
+            this.Controls.Add(this.additionButton);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.resultTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.bImaginaryTextBox);
+            this.Controls.Add(this.bRealTextBox);
+            this.Controls.Add(this.operationLabel);
+            this.Controls.Add(this.aImaginaryTextBox);
+            this.Controls.Add(this.aRealTextBox);
+            this.Name = "Form1";
+            this.Text = "Kalkulator liczb zespolonych, stworzony do prostych działań, takich jak dodawanie" +
+    " i mnożenie";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox aRealTextBox;
+        private System.Windows.Forms.TextBox aImaginaryTextBox;
+        private System.Windows.Forms.Label operationLabel;
+        private System.Windows.Forms.TextBox bRealTextBox;
+        private System.Windows.Forms.TextBox bImaginaryTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox resultTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button additionButton;
+        private System.Windows.Forms.Button substractionButton;
+        private System.Windows.Forms.Button multiplicationButton;
+        private System.Windows.Forms.Button divisionButton;
+        private System.Windows.Forms.Button biggerDistanceButton;
+        private System.Windows.Forms.Button samallerDistanceButton;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button hashButton;
+        private System.Windows.Forms.Button equelsButton;
     }
 }
 
